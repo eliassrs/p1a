42b3e83df458e8e5907cdf5018047471a84f2708
Elias Moreira <moreira-s@hotmail.com>
2021-03-28 17:11:49 -0300

    lalala

---
+++ src/main/java/p1a/Main.java
@@ -0,0 +1,17 @@
+package p1a;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Main app = new Main();
+		app.start();
+		
+
+	}
+
+	private void start() {
+		System.out.println("lalala");
+		
+	}
+
+}
